b13818f902a649ab518175ecdfd926915de021d2
LJJordan124 <jordan3313.lj@gmail.com>
2023-08-01 09:50:40 -0400

    ReRouting
    Fixed my issues from another repository, so went through my mistakes, cleaned it up a bit, and Voila

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,117 +1,35 @@
-//Dependencies
-const path = require('path');
-const fs = require('fs');
+const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const fs = require ("fs");
 
-module.exports = function (app) {
-  //GET route to serve the HTML page for displaying notes
-  app.get('/api/notes', (req, res) => {
-    fs.readFile(
-      path.join(__dirname, '..', 'db', 'db.json'),
-      'utf8',
-      (err, data) => {
-        if (err) {
-          console.error(err);
-          return res
-            .status(500)
-            .json({ error: 'Failed to read data from the database.' });
-        }
-        return res.json(data);
-      }
-    );
+// Defines the get request to this routes end point '/api/notes'
+router.get('/api/notes', async (req, res) => {
+  const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  res.json(dbJson);
+});
+
+// Defines the post request to this routes end point '/api/notes'
+router.post('/api/notes', (req, res) => {
+  const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dbJson.push(newFeedback);
+  fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+  res.json(dbJson);
+});
+
+// Defines the delete request to this routes end point '/api/notes/:id'
+router.delete('/api/notes/:id', (req, res) => {
+  let data = fs.readFileSync("db/db.json", "utf8");
+  const dataJSON =  JSON.parse(data);
+  const newNotes = dataJSON.filter((note) => { 
+    return note.id !== req.params.id;
   });
+  fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
+  res.json("Note deleted.");
+});
 
-  //POST route for a new note
-  app.post('/api/notes', (req, res) => {
-    // Read the existing notes from the database file (db.json)
-    fs.readFile(
-      path.join(__dirname, '..', 'db', 'db.json'),
-      'utf8',
-      (err, data) => {
-        if (err) {
-          console.error(err);
-          return res
-            .status(500)
-            .json({ error: 'Failed to read data from the database.' });
-        }
-
-        // Parse the JSON data coming from db into an array
-        const notes = JSON.parse(data);
-
-        // Create a new note object with a unique ID
-        const newNote = {
-          id: uuidv4(),
-          title: req.body.title,
-          text: req.body.text,
-        };
-
-        // Add the new note to the array
-        notes.push(newNote);
-
-        // Write the updated notes array back to the database file
-        fs.writeFile(
-          path.join(__dirname, '..', 'db', 'db.json'),
-          JSON.stringify(notes),
-          (err) => {
-            if (err) {
-              console.error(err);
-              return res
-                .status(500)
-                .json({ error: 'Failed to save the note to the database.' });
-            }
-
-            // Respond with the newly created note
-            res.json(newNote);
-          }
-        );
-      }
-    );
-  });
-
-  //DELETE route to delete note by ID (BONUS)
-  app.delete('/api/notes/:id', (req, res) => {
-    // Read the existing notes from the database file (db.json)
-    fs.readFile(
-      path.join(__dirname, '..', 'db', 'db.json'),
-      'utf8',
-      (err, data) => {
-        if (err) {
-          console.error(err);
-          return res
-            .status(500)
-            .json({ error: 'Failed to read data from the database.' });
-        }
-
-        // Parse the JSON data into an array
-        const notes = JSON.parse(data);
-
-        // Find the index of the note with the specified ID
-        const noteIndex = notes.findIndex((note) => note.id === req.params.id);
-
-        if (noteIndex === -1) {
-          return res.status(404).json({ error: 'Note not found.' });
-        }
-
-        // Remove the note from the array
-        notes.splice(noteIndex, 1);
-
-        // Write the updated notes array back to the database file
-        fs.writeFile(
-          path.join(__dirname, '..', 'db', 'db.json'),
-          JSON.stringify(notes),
-          (err) => {
-            if (err) {
-              console.error(err);
-              return res.status(500).json({
-                error: 'Failed to delete the note from the database.',
-              });
-            }
-
-            // Respond with a success message
-            res.json({ message: 'Note deleted successfully.' });
-          }
-        );
-      }
-    );
-  });
-};
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,13 @@
-const path = require('path');
 const router = require('express').Router();
+const path = require('path');
 
-//GET notes.html
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+// Defines the route that sends 'index.html' as a response to a client when a GET request is made
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-
-//GET index.html
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+// Defines the route that sends 'notes.html" as a response to a client when a GET request is made 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
 module.exports = router;
